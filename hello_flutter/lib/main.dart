ba5273f16c9a3dd6b685c0513c11051d03aa48d2
gongbaodd@Inspiron <gongbaodd@qq.com>
2020-02-21 12:29:47 +0800

    add dependencies

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
         title: "Welcome to my app",
         home: Scaffold(
           appBar: AppBar(
             title: Text("Welcom to Flutter"),
           ),
-          body: Center(child: Text("Hello World")),
+          body: Center(child: Text(wordPair.asPascalCase)),
         ));
   }
 }
