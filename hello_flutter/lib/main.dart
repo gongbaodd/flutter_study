b1c3d5c5aa1bd06be34e65fb7bf8dbcc97acd068
gongbaodd@Inspiron <gongbaodd@qq.com>
2020-02-21 13:25:42 +0800

    infinite scroll

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -6,21 +6,39 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "Welcome to my app",
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Welcom to Flutter"),
-          ),
-          body: Center(child: RandomWords()),
-        ));
+    return MaterialApp(title: "Welcome to my app", home: RandomWords());
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFront = const TextStyle(fontSize: 18.0);
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(title: Text(pair.asLowerCase, style: _biggerFront));
+  }
+
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+        appBar: AppBar(title: Text('Startup Name Generator')),
+        body: _buildSuggestions());
   }
 }
 
