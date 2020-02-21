596c4bba134bfaee192569500c6f0b412cdb5369
gongbaodd@Inspiron <gongbaodd@qq.com>
2020-02-21 12:37:38 +0800

    state machine

--- hello_flutter/lib/main.dart
+++ hello_flutter/lib/main.dart
@@ -4,15 +4,27 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
         title: "Welcome to my app",
         home: Scaffold(
           appBar: AppBar(
             title: Text("Welcom to Flutter"),
           ),
-          body: Center(child: Text(wordPair.asPascalCase)),
+          body: Center(child: RandomWords()),
         ));
   }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
